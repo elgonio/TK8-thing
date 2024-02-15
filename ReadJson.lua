3a215b2593da962555ec0e8c2533be1251a7ffc4
Omer Elgoni <omer.elgoni@gmail.com>
2024-02-15 22:56:05 +0200

    Initial Commit

---
+++ ReadJson.lua
@@ -0,0 +1,46 @@
+function writeFile(filePath,data)
+    if (type(data)=='string') then
+       local file = assert(io.open(filePath,'a'),'io.open failed!'); -- create files if does not exists and appends to it;
+       file:write(data);
+       file:close();
+       return true
+    end
+    return false
+ end
+ 
+ local process = openProcess('Polaris-Win64-Shipping')
+ 
+ pause()
+ -- Create a memory scanner
+ local memscan = createMemScan(process)
+ 
+ -- Set the scan options
+ memscan.firstScan(soExactValue, vtString, nil, '{"replayDetailList":', '', 0, 0xffffffffffffffff, '', fsmNotAligned, '', false, false, false, true)
+ 
+ -- Wait for the scan to complete
+ memscan.waitTillDone()
+ 
+ local foundlist = createFoundList(memscan)
+ foundlist.initialize()
+ 
+ for i = 0, 100 do
+
+    for i = 0, foundlist.getCount() - 1 do
+        -- Get the address of the result
+        local address = foundlist.getAddress(i)
+    
+        -- Read the string from memory
+        local result = readString(address, 2000000)  -- Read up to 50 characters
+    
+        -- Print the result
+        print('-----')
+        print('Found at address')
+        print(address)
+        print(result)
+    
+        writeFile('D:/coding stuff/CE/' .. os.time() ..'_' .. i ..'.json', result)
+    end
+
+    sleep(180000)
+
+end
